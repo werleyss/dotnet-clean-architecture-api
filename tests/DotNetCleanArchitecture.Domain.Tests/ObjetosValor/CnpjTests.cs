eb009f79ba0ec95574f6f2580ca57c48e5522f57
werleyss <ssley15@gmail.com>
2026-08-26 22:18:57 -0300

    test: adiciona testes do cnpj

---
+++ tests/DotNetCleanArchitecture.Domain.Tests/ObjetosValor/CnpjTests.cs
@@ -0,0 +1,44 @@
+﻿using DotNetCleanArchitecture.Domain.Core.Excecoes;
+using DotNetCleanArchitecture.Domain.Core.ObjetosValor;
+
+namespace DotNetCleanArchitecture.Domain.Tests;
+
+public class CnpjTests
+{
+    [Fact]
+    public void Deve_criar_cnpj_valido()
+    {
+        // Act
+        var cnpj = Cnpj.Criar("00.000.000/E08G-12");
+
+        // Assert
+        Assert.NotNull(cnpj);
+        Assert.Equal("00000000E08G12", cnpj.Numero);
+    }
+
+    [Fact]
+    public void Deve_impedir_cnpj_invalido()
+    {
+        // Act
+        var excecao = Assert.Throws<ExcecaoDeDominio>(() =>
+            Cnpj.Criar("00.000.000/E08G-11"));
+
+        // Assert
+        Assert.Equal(
+            "CNPJ inválido.",
+            excecao.Message);
+    }
+
+    [Fact]
+    public void Deve_impedir_cnpj_vazio()
+    {
+        // Act
+        var excecao = Assert.Throws<ExcecaoDeDominio>(() =>
+            Cnpj.Criar(""));
+
+        // Assert
+        Assert.Equal(
+            "O CNPJ deve ser informado.",
+            excecao.Message);
+    }
+}
